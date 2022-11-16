1154d35cba501137c8ada60fcfe3b6647c3801a0
MarioPovil <marioapa2@hotmail.com>
2022-11-16 17:41:24 -0600

    Curriculum v.1

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,37 +1,41 @@
 import React from 'react'
-
+import { useState } from 'react';
+import { usePopper } from 'react-popper';
 function Body() {
+  
+
   return (
     <>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
           <div class="container px-4 px-lg-5">
-              <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+              <a class="navbar-brand" href="#page-top">Mario Povil</a>
               <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
               <div class="collapse navbar-collapse" id="navbarResponsive">
                   <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                      <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                      <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                      <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-                      <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#about"> Estudios</a></li>
+                      <li class="nav-item"><a class="nav-link" href="#services">Conocimientos </a></li>
+                      <li class="nav-item"><a class="nav-link" href="#portfolio">Proyectos</a></li>
+                      <li class="nav-item"><a class="nav-link" href="#contact">Redes</a></li>
+
+
                   </ul>
               </div>
           </div>
       </nav><header class="masthead">
               <div class="container px-4 px-lg-5 h-100">
                   <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                      <div class="col-lg-8 align-self-end">
-                          <h1 class="text-white font-weight-bold">¡Un placer! soy Mario Ponce</h1>
-                          <h2 class="text-white font-weight-bold">Me pueden encontarar como Mario Povil en <a href='https://github.com/MarioPovil'><img style={{width:"3rem"}} src='https://cdn4.iconfinder.com/data/icons/socialcones/508/Github-128.png'/>
+                      <div class="col-lg-8 align-self-end mb-4 ">
+                          <h1 class="text-white font-weight-bold">¡Un placer soy Mario Ponce!</h1> <br/>
+                          <h2 class="text-white font-weight-bold">Me encuentro como Mario Povil en <a href='https://github.com/MarioPovil'><img style={{width:"2.5rem"}} src='https://cdn4.iconfinder.com/data/icons/socialcones/508/Github-128.png'/>
                            </a>
-                          <br></br> Me encuentro estudiando Desarrollo Web FullStack, y por egresar de Universidad UTEL. </h2>
+                          <br></br>  Me encuentro estudiando Desarrollo Web FullStack, y por egresar de Universidad UTEL. </h2><br/>
                           <hr class="divider" />
-                      </div>
-                      <div class="col-lg-8 align-self-baseline">
-                      <h1 class="text-white font-weight-bold">¡Nice to meet you! I'm Mario Ponce</h1>
-                          <h2 class="text-white font-weight-bold">You can find me as Mario Povil in my GIT <a href='https://github.com/MarioPovil'><img style={{width:"3rem"}} src='https://cdn4.iconfinder.com/data/icons/socialcones/508/Github-128.png'/>
-                           </a>
-                          <br></br>I'm studing FullStack Web development, and nearly graduated from UTEL University. </h2>
-                         
+                          <h3> Apasionado autodidacta, me encanta aprender cosas nuevas, y mantenerme actualizado. <br/>
+                                Cooperativo, me gusta mucho el trabajo en equipo, y más en equipos a largo plazo. <br/>
+                                Contendiente, por lo cual me apasionan los trabajos retadores, y el aprendizaje 
+                                nuevo, para poder mejorar, ampliar, y refinar mis habilidades en programación. <br/>
+                                <span style={{color:"#AAFFDD"}}>  Sin reinventar la rueda, pero si sabiendo como es que esta puede rodar. </span> </h3>
+                                
                       </div>
                   </div>
               </div>
@@ -40,67 +44,87 @@ function Body() {
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">Actualmente </h2>
+                        <h2 class="text-white mt-0">Estudios </h2>
                         <hr class="divider divider-light" />
-                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
+                        <ul> 
+                        <li><h3 class="text-white-75 mb-4"> Licenciatura en Ingenieria en Sistemas Computacionales en Universidad UTEL <br/>
+                       <div class="fw-bold ">( Actualmente Estudiando ) </div></h3>  </li>
+                       <br/>
+                        <li><h3 class="text-white-75 mb-4"> Ingenieria en Mecatronica En ITESRC <br/>(Instituo Tecnologico de La Región Carbonifera) 
+                        <div class="fw-bold ">( Carrera concluida ) </div> </h3> </li>                       
+                        </ul>
                     </div>
                 </div>
             </div>
         </section>
         <section class="page-section" id="services">
             <div class="container px-4 px-lg-5">
-                <h2 class="text-center mt-0">My Knowledges</h2>
+                <h2 class="text-center mt-0">Conocimientos</h2>
                 <hr class="divider" />
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5" style={{background:"#DEEEEE", borderRadius:"15px 2px 15px 2px"}}>
+                        <div class="mt-5" style={{background:"#DEEEEE", borderRadius:"15px 2px 15px 2px"}}> <br/>
                             <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
                             <h3 class="h4 mb-2">Program Lenguages</h3>
                             <div class="grid "> <img src='https://cdn1.iconfinder.com/data/icons/ionicons-fill-vol-2/512/logo-javascript-128.png' style={{width:'50px'}}/>
-                            <p class="text-muted mb-0"> it's my main lenguage in Frontend</p> 
+                            <p class="text-muted mb-0"> Es mi lenguaje preferido en frontend</p> 
                             </div>
                             <div  > <img src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-128.png' style={{width:'50px'}}/>
-                            <p class="text-muted mb-0"> And Java it's my main lenguage in Backend</p> 
-                            </div>
+                            <p class="text-muted mb-0"> y Java mi lenguaje preferido en Backend</p> 
+                            <br/>  </div>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5" style={{background:"#EEDEEE", borderRadius:"15px 2px 15px 2px"}}>
+                        <div class="mt-5" style={{background:"#EEDEEE", borderRadius:"15px 2px 15px 2px"}}> <br/>
                             <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
                             <h3 class="h4 mb-2">Frameworks</h3>
                             <div class="grid "> <img src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-128.png' style={{width:'50px'}}/>
                            <img src='https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react-128.png' style={{width:'50px'}}/>
-                            <p class="text-muted mb-0"> I use most communly React, and Express, but i'd like to keep learning Angular, and Vue</p> 
+                            <p class="text-muted mb-0">Uso más frecuentemente React, pero me gustaría poder especializarme más en Angular y Vue</p> 
                             </div>
                             <div > <img src='https://cdn2.iconfinder.com/data/icons/boxicons-logos/24/bxl-spring-boot-128.png' style={{width:'50px'}}/>
-                            <p class="text-muted mb-0"> In Java my unique Framework is Spring Boot at the moment.</p> 
-                            </div>
+                            <p class="text-muted mb-0"> Por el momento en Java solo utilizo de framework Spring Boot</p> 
+                            <br/></div>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5" style={{background:"#EEEEDE", borderRadius:"15px 2px 15px 2px"}}>
+                        <div class="mt-5" style={{background:"#EEEEDE", borderRadius:"15px 2px 15px 2px"}}> <br/>
                             <div class="mb-2"><img src='https://cdn4.iconfinder.com/data/icons/web-basics-vol-05/512/data_database_mysql_server_sql_storage-128.png' style={{width:'45px'}}/></div>
                             <h3 class="h4 mb-2">DataBases</h3>
                             <div class="grid "> <img src='https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/mysql-128.png' style={{width:'50px'}}/>
-                            <p class="text-muted mb-0"> For SQL DB's i like to use MySQL, with MERN stack.</p> 
+                            <p class="text-muted mb-0"> Para bases de datos SQl, me gusta usar MySQL</p> 
                             </div>
                             <div  > <img src='https://cdn1.iconfinder.com/data/icons/soleicons-fill-vol-1/64/postgres_database_server_relational_dbms_sql-128.png' style={{width:'50px'}}/>
                             <img src='https://cdn4.iconfinder.com/data/icons/logos-3/512/mongodb-2-128.png'/>
-                            <p class="text-muted mb-0"> In NoSQL DB's i use Postgres and MongoDB with <span style={{color:"blue"}}> Docker.</span></p> 
-                            </div>
+                            <p class="text-muted mb-0"> En NoSQL uso comunmente Postgres, y MongoDB, junto con  <span style={{color:"blue"}}> Docker.</span></p> 
+                            <br/>  </div>
                         </div>
                     </div>
                     
                     <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5" style={{background:"#CDDECC", borderRadius:"15px 2px 15px 2px"}}>
                         <div class="mt-5">
                             <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
-                            <h3 class="h4 mb-2">Made with Love</h3>
-                            <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                            <ul> 
+
+                            <h3 class="h4 mb-2">Idiomas</h3> <br/>
+                            <div class="bg-secondary">
+                            <h3 style={{color:"#FED"}} > Español</h3><br/>
+                            <p id='idioms'>Lengua Materna</p> <br/>
+                            </div>
+                            <div class=" bg-secondary">
+                             <h3 style={{color:"#FED"}}> Ingles</h3>  <br/>
+                            <p id='idioms'> Speaking: Advance 
+                              Reading: Advance <br/> 
+                              Listening: Advance  </p>     
+                             </div> <br/>
+                              </ul>
+                        </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </section>
@@ -109,7 +133,7 @@ function Body() {
                 <div class="row g-0">
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="./src/assets/img/portfolio/fullsize/1.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
+                            <img class="img-fluid" src="assets/img/PERN1.png" alt="PERN stack" />
                             <div class="portfolio-box-caption">
                                 <div class="project-category text-white-50">Category</div>
                                 <div class="project-name">Project Name</div>
@@ -117,7 +141,7 @@ function Body() {
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
+                        <a class="portfolio-box" href="assets/img/PernCrud.png" title="Project Name">
                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="..." />
                             <div class="portfolio-box-caption">
                                 <div class="project-category text-white-50">Category</div>
@@ -126,36 +150,16 @@ function Body() {
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
+                        <a class="portfolio-box" href="assets/img/tictactoe.png" title="Project Name">
                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
                             <div class="portfolio-box-caption">
                                 <div class="project-category text-white-50">Category</div>
                                 <div class="project-name">Project Name</div>
                             </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
+                            <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/tictactoecode.png" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
-                            <div class="portfolio-box-caption p-3">
                                 <div class="project-category text-white-50">Category</div>
                                 <div class="project-name">Project Name</div>
                             </div>
@@ -166,8 +170,8 @@ function Body() {
         </div>
         <section class="page-section bg-dark text-white">
             <div class="container px-4 px-lg-5 text-center">
-                <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-                <a class="btn btn-light btn-xl" href="https://startbootstrap.com/theme/creative/">Download Now!</a>
+                <h2 class="mb-4">Proyectos</h2>
+                <a class="btn btn-light btn-xl" href="https://github.com/MarioPovil?tab=repositories" target="_blank">Github</a>
             </div>
         </section>
          <section class="page-section" id="contact">
